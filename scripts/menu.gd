3c9ce9ae623e34a703ad74e6a284fe6f0fad11df
Somebud0180 <ethanlagera@icloud.com>
2025-11-04 23:35:34 +0800

    Completed the dynamic UI for the flag mode sidebar, made improvements to the detection to reduce unnecessary calls

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var animation_player: AnimationPlayer
+@export var sidebar_animation_player: AnimationPlayer
 @export var resume_button: TextureButton
 @export var start_button: TextureButton
 
@@ -14,6 +15,8 @@ var in_game: bool:
 		in_game = value
 		resume_button.visible = in_game
 		start_button.texture_normal = new_game_btntext if in_game else start_game_btntext
+
+var is_paused: bool = false
 
 var music_vol: float = 1.0
 var sfx_vol: float = 1.0
@@ -32,6 +35,9 @@ func _on_start_button_pressed() -> void:
 
 func _on_resume_button_pressed() -> void:
 	hide_and_show("main", "game")
+	is_paused = false
+	if Globals.input_type != 0:
+		sidebar_animation_player.play("shared_animations/show_flag_mode")
 
 func _on_settings_button_pressed() -> void:
 	hide_and_show("main", "settings")
@@ -58,6 +64,9 @@ func _on_difficulty_button_pressed(difficulty: int) -> void:
 	
 	game_scene.start()
 	hide_and_show("difficulty", "game")
+	is_paused = false
+	if Globals.input_type != 0:
+		sidebar_animation_player.play("shared_animations/show_flag_mode")
 
 # Settings Functions
 func _on_button_pressed() -> void:
@@ -71,26 +80,28 @@ func _on_sfx_volume_value_changed(value: float) -> void:
 
 func update_flag_mode() -> void:
 	# If Keyboard and Mouse, catch 
+	print("Updating Flag Mode; Input type: {input_type}".format({"input_type": Globals.input_type}))
 	if Globals.input_type == 0:
-		if get_node_or_null("GameLayer/TileModeOverlay"):
-			%GameLayer/TileModeOverlay.free()
+		sidebar_animation_player.play("shared_animations/hide_flag_mode")
 		return
 	
-	if Globals.flag_mode == 0 and get_node_or_null("GameLayer/TileModeOverlay"):
-		animation_player.play("hide_flag_mode")
-		await animation_player.animation_finished
-		%GameLayer/TileModeOverlay.free()
-	elif Globals.flag_mode == 1 and !get_node_or_null("GameLayer/TileModeOverlay"):
-		var new_overlay = tile_mode_overlay.instantiate()
-		%GameLayer.add_child(new_overlay)
-		%GameLayer/TileModeOverlay.visible = false
-		animation_player.play("show_flag_mode")
+	print("Updating Flag Mode")
+	if Globals.flag_mode == 0:
+		sidebar_animation_player.play("shared_animations/hide_flag_mode")
+	elif Globals.flag_mode == 1 and in_game and !is_paused:
+		sidebar_animation_player.play("shared_animations/show_flag_mode")
 
 func _input(event: InputEvent) -> void:
+	var new_input_type = 0
 	if event is InputEventScreenTouch:
-		Globals.input_type = 1
-	elif event is InputEventJoypadButton:
-		Globals.input_type = 2
-		print("Controller Detected")
+		new_input_type = 1
+	elif event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		new_input_type = 2
 	else:
-		Globals.input_type = 0
+		new_input_type = 0
+	
+	# Reduce unnecessary updates
+	if Globals.input_type == new_input_type:
+		return
+	else:
+		Globals.input_type = new_input_type
