4e9c905a839832ebddae30bf626e4845c4b63a23
Somebud0180 <ethanlagera@icloud.com>
2025-11-28 00:02:10 +0800

    Updated pause and back button textures, added new textures for the flag mode sidebar buttons

--- scripts/pause_button.gd
+++ scripts/pause_button.gd
@@ -4,9 +4,40 @@ var main_screen
 
 func _ready() -> void:
 	main_screen = get_tree().get_first_node_in_group("MainScreen")
+	connect("button_down", _press_button)
+	connect("button_up", _normalize_button)
+	connect("focus_entered", _enlarge_button)
+	connect("focus_exited", _normalize_button)
+	connect("mouse_entered", _enlarge_button)
+	connect("mouse_exited", _normalize_button)
 	
+	get_tree().root.connect("size_changed", _on_viewport_size_changed)
+	pivot_offset = Vector2(size.x/2, size.y/2)
+
 func _on_pressed() -> void:
 	if main_screen:
 		main_screen.hide_and_show("game", "main")
 		if main_screen.sidebar_visible:
 			main_screen.sidebar_animation_player.play("shared_animations/hide_flag_mode")
+
+func _on_viewport_size_changed() -> void:
+	pivot_offset = Vector2(size.x/2, size.y/2)
+
+# Animations
+func _enlarge_button() -> void:
+	z_index = 0
+	z_index += 1
+	$ButtonAnimations.play("enlarge")
+	Globals.vibrate_stop()
+	Globals.vibrate_hover()
+
+func _normalize_button() -> void:
+	$ButtonAnimations.play("normalize")
+	await $ButtonAnimations.animation_finished
+	z_index = 0
+
+func _press_button() -> void:
+	$ButtonAnimations.play("press")
+	Globals.vibrate_hard_press()
+	await $ButtonAnimations.animation_finished
+	z_index = 0
--- scripts/tile_mode_overlay.gd
+++ scripts/tile_mode_overlay.gd
@@ -1,12 +1,31 @@
 extends VBoxContainer
 
+const REVEAL_TEXT = preload("res://interface/textures/ButtonTexture/SidebarButtons/RevealButton.png")
+const REVEAL_F_TEXT = preload("res://interface/textures/ButtonTexture/SidebarButtons/RevealButton_Focus.png")
+const FLAG_TEXT = preload("res://interface/textures/ButtonTexture/SidebarButtons/FlagButton.png")
+const FLAG_F_TEXT = preload("res://interface/textures/ButtonTexture/SidebarButtons/FlagButton_Focus.png")
+
 func _on_reveal_mode_pressed() -> void:
 	Globals.is_flagging = false
+	_update_button_text()
 
 func _on_flag_mode_pressed() -> void:
 	Globals.is_flagging = true
+	_update_button_text()
 
 func _input(event: InputEvent) -> void:
 	if event.is_pressed():
 		if event.is_action_pressed("toggle_flag"):
 			Globals.is_flagging = !Globals.is_flagging
+			_update_button_text()
+
+func _ready() -> void:
+	_update_button_text()
+
+func _update_button_text() -> void:
+	if Globals.is_flagging:
+		$RevealMode.texture_normal = REVEAL_TEXT
+		$FlagMode.texture_normal = FLAG_F_TEXT
+	else:
+		$RevealMode.texture_normal = REVEAL_F_TEXT
+		$FlagMode.texture_normal = FLAG_TEXT
